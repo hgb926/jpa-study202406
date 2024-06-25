6417015bc8655da2a0d1609b5a004295c1c7421a
hgb926 <hgb926@naver.com>
2024-06-25 09:38:09 +0900

    initial commit

---
+++ src/test/java/com/spring/jspstudy/JspstudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.jspstudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JspstudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
