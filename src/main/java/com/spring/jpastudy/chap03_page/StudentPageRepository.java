cef7722b6ee2c6ed4bb7ce16a2f4e0c921eb09a3
hgb926 <hgb926@naver.com>
2024-06-26 09:58:44 +0900

    0626 페이징 처리

---
+++ src/main/java/com/spring/jpastudy/chap03_page/StudentPageRepository.java
@@ -0,0 +1,22 @@
+package com.spring.jpastudy.chap03_page;
+
+import com.spring.jpastudy.chap02.entity.Student;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+
+public interface StudentPageRepository extends JpaRepository<Student, String> {
+
+    // 전체조회 상황에서 페이징 처리하기
+
+    // Import해온 Page 클래스에는 필요한 것들이 이미 다 구현되어 있다.
+    Page<Student> findAll(Pageable pageable);
+
+    // 검색 + 페이징
+    Page<Student> findByNameContaining(String name, Pageable pageable);
+
+
+}
